f73aba45a46a15e6948cbc7a97790479fa5f6af6
sergio <frikiland2@gmail.com>
2015-04-29 20:50:58 +0200

    Subasta
    ASDASD

--- project/ret2/app/Http/Controllers/Subasta.php
+++ project/ret2/app/Http/Controllers/Subasta.php
@@ -1,5 +1,19 @@
 <?php namespace App\Http\Controllers;
 
+use App\Http\Controllers\AdminController;
+use App\Photo;
+use App\PhotoAlbum;
+use App\Language;
+use App\Http\Requests\Admin\PhotoRequest;
+use App\Http\Requests\Admin\DeleteRequest;
+use App\Http\Requests\Admin\ReorderRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Input;
+use App\Helpers\Thumbnail;
+use Illuminate\Support\Facades\DB;
+use Datatables;
+use Illuminate\Html\HtmlBuilder;
+
 class Subasta extends Controller {
 
 	public function __construct()
