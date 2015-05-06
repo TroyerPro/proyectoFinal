8fc8f0c020cad7c5d034a2381aec9c2b6db7345f
amdadria <amdadria@hotmail.com>
2015-05-06 18:39:06 +0200

    search general con ampliacion en click

--- project/ret2/resources/views/search/user.blade.php
+++ project/ret2/resources/views/search/user.blade.php
@@ -1,9 +1,20 @@
 @extends('app')
-@section('title') About :: @parent @stop
+@section('title') Contact :: @parent @stop
 @section('content')
-    <div class="row">
-        <div class="page-header">
-            <h2>Buscador de Usuario</h2>
+<div class="page-header">
+    <h2>Buscador de Usuarios</h2>
+</div>
+<div id="ResultItems" class="">
+  <ul id="ListView">
+    @foreach ($user as $user)
+      <li id='{{ $user->id }}'>
+        <div class="">
+          <a href="#">imagen del user</a>
         </div>
-    </div>
-@endsection
+        <h3>{{ $user->name }}</h3>
+      </li>
+    @endforeach
+  </ul>
+</div>
+
+@stop
