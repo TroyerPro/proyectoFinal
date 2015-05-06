893684f33bea8e79477dfa37dd45ab8e0f5f26b5
amdadria <amdadria@hotmail.com>
2015-05-06 15:05:38 +0200

    desplegable search

--- project/ret2/resources/views/partials/nav.blade.php
+++ project/ret2/resources/views/partials/nav.blade.php
@@ -13,15 +13,19 @@
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-                <li class="{{ (Request::is('/') ? 'active' : '') }}">
-                    <a href="{!! URL::to('') !!}"><i class="fa fa-home"></i> Inicio</a>
-                </li>
-                <li class="{{ (Request::is('about') ? 'active' : '') }}">
-                    <a href="{!! URL::to('about') !!}">Acerca de nosotros</a>
-                </li>
-                <li class="{{ (Request::is('contact') ? 'active' : '') }}">
-                    <a href="{!! URL::to('contact') !!}">Contacto</a>
-                </li>
+              <li class="{{ (Request::is('/') ? 'active' : '') }}">
+                  <a href="{!! URL::to('') !!}"><i class="fa fa-home"></i> Inicio</a>
+              </li>
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+                   aria-expanded="false"><i class="fa fa-search"></i>Buscar</a>
+                  <ul class="dropdown-menu" role="menu">
+                    <li>
+                    </li>
+                    <li>
+                    </li>
+                  </ul>
+              </li>
             </ul>
 
             <ul class="nav navbar-nav navbar-right">
