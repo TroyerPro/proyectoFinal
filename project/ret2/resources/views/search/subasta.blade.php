c1dbf8bd40b9243b0fd625a3d402b9cf9b43443e
amdadria <amdadria@hotmail.com>
2015-05-06 15:08:36 +0200

    a
    a

---
+++ project/ret2/resources/views/search/subasta.blade.php
@@ -0,0 +1,9 @@
+@extends('app')
+@section('title') About :: @parent @stop
+@section('content')
+    <div class="row">
+        <div class="page-header">
+            <h2>buscador subasta</h2>
+        </div>
+    </div>
+@endsection
