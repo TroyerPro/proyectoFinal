76adaa3b87873c2661bccac926927781e593d7eb
amdadria <amdadria@hotmail.com>
2015-05-07 17:37:15 +0200

    gerfs

--- project/ret2/resources/views/search/user.blade.php
+++ project/ret2/resources/views/search/user.blade.php
@@ -1,20 +1,42 @@
 @extends('app')
 @section('title') Contact :: @parent @stop
 @section('content')
-<div class="page-header">
+<div class="page-header col-xs-12">
     <h2>Buscador de Usuarios</h2>
 </div>
-<div id="ResultItems" class="">
-  <ul id="ListView">
-    @foreach ($user as $user)
-      <li id='{{ $user->id }}'>
-        <div class="">
-          <a href="user/view/{{ $user->id }}">imagen del user</a>
+<div class="row">
+  <div class="col-xs-12">
+    <div class="col-xs-12">
+      @yield('main')
+        <div  class="col-xs-3" id="buscador">
+          <div class="input-group">
+              <input type="text" class="form-control" placeholder="Buscar...">
+                <span class="input-group-btn">
+                  <button class="btn btn-default" type="button">
+                      <i class="fa fa-search"></i>
+                  </button>
+                </span>
+          </div>
         </div>
-        <h3>{{ $user->name }}</h3>
-      </li>
-    @endforeach
-  </ul>
+      <div class="col-xs-9">
+            @foreach ($user as $user)
+            <div class="col-xs-12" style="border-bottom:solid grey 1px;margin-top:2%;">
+                <div class="col-xs-5">
+                  <a href="user/view/{{ $user->id }}">imagen del user</a>
+                </div>
+                <div class="col-xs-3">
+                  <h4>{{ $user->name }}</h4>
+                </div>
+                <div class="col-xs-4">
+                  <h4>{{ $user->email }}</h4>
+                </div>
+
+            </div>
+            @endforeach
+      </div>
+    </div>
+  </div>
 </div>
 
-@stop
+
+@endsection
--- project/ret2/resources/views/subasta/view.blade.php
+++ project/ret2/resources/views/subasta/view.blade.php
@@ -10,37 +10,38 @@
           @yield('main')
          <div class="col-xs-6 under_panel">
            <div class="col-xs-12">
+             <h4>{{ $subasta->nombre }}</h4>
+           </div>
+           <div class="col-xs-12">
              <img class="imagensubasta" src="{{ $subasta->imagen }}">
+             ((foto producto))
            </div>
            <div class="col-xs-12">
              Pujas:
            </div>
-           <div class="col-xs-6">
+           <div class="col-xs-12">
             Puja actual: {{ $subasta->precio_actual }}
            </div>
-           <div class="col-xs-6">
-          <!--  Usuario: {{ $subasta->id_user_vendedor }}(cambiar por name)-->
-           </div>
-           <div class="col-xs-4">
+           <div class="col-xs-12">
               <button class="btn btn-default">Pujar</button>
            </div>
          </div>
          <div class="col-xs-6 under_panel">
            <div class="col-xs-12 ">
-             <div class="col-xs-6">
-               Producto: <div_nombre>{{ $subasta->nombre }}</div_nombre>
-             </div>
-             <div class="col-xs-6">
-               Acaba: <div_fecha>{{ $subasta->fecha_final }}<div_fecha>
-             </div>
-             <div class="col-xs-6 ">
-               Nombre usuario: <div_usuario>{{ $user->name }}</div_usuario>
-             </div>
-             <div class="col-xs-6 ">
-               Rating como vendedor: <div_rating>{{ $user->ratingvendedor }}</div_raint>
+             <div class="col-xs-12">
+               <b>Fecha final:</b> <div_fecha>{{ $subasta->fecha_final }}<div_fecha>
              </div>
              <div class="col-xs-12 ">
-               Descripción del Producto: <div_desc>{{ $subasta->descripcion }}</div_desc>
+               <b>Usuario vendedor:</b> <div_usuario>{{ $user->name }}</div_usuario>
+             </div>
+             <div class="col-xs-12">
+               <b>Rating del vendedor:</b> <div_rating>{{ $user->ratingvendedor }}</div_raint>
+             </div>
+             <div class="col-xs-12 ">
+               <b>Descripción del Producto</b>
+             </div>
+             <div class="col-xs-12">
+               <div_desc>{{ $subasta->descripcion }}</div_desc>
              </div>
            </div>
          </div>
