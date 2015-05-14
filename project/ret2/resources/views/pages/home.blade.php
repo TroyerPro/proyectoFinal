42b8b8f5dbe4ffac0f9afdec555901dfd687b4b5
amdadria <amdadria@hotmail.com>
2015-05-14 13:23:53 +0200

    home

--- project/ret2/resources/views/pages/home.blade.php
+++ project/ret2/resources/views/pages/home.blade.php
@@ -25,45 +25,56 @@
   @section('content')
   <div class="row">
     <div class="page-header">
-      <h2>Inicio</h2>
+      <h2>RET</h2>
     </div>
-    <div class="col-xs-12 navbar-inverse main_panel">
-      <div class="col-xs-12 products" >
-        <!--CARUSEL -->
-        <div id="slider1">
-          <a class="buttons prev" href="#">&#60;</a>
-          <div class="viewport">
-            <ul class="overview">
-              @foreach ($bid as $bid)
-              <li>
-                <a href="search/subasta/view/{{ $bid->id }}">
-                  <img src="{{ URL::asset('img/subasta/'.$bid->imagen) }}" alt="" class="main_image"/>
-                </a>
-              </li>
-              @endforeach
-            </ul>
-          </div>
-          <a class="buttons next" href="#">&#62;</a>
-        </div>
-      </div>
-
-
-      <div class="col-xs-3">
-        <div class="col-xs-12" class="small_row">
-          <h2 class="text-center">Categorias</h2>
-        </div>
-
+    <div class="col-xs-12 ">
+      <div class="col-xs-3" >
+        <h2>Categorias</h2>
         @foreach ($categoria as $categoria)
-        <div class="col-xs-12" class="small_row">
+        <div class="col-xs-" class="small_row">
           <a href="search/subasta/filtro/{{ $categoria->id }}">
             {{ $categoria->nombre }}
           </a>
         </div>
         @endforeach
+      </div>
+      <div class="col-xs-8 products">
+        <div class="col-xs-12 products" >
+          <!--CARUSEL -->
+          <div id="slider1">
+            <a class="buttons prev" href="#">&#60;</a>
+            <div class="viewport">
+              <ul class="overview">
+                @foreach ($bid as $bid)
+                <li>
+                  <a href="search/subasta/view/{{ $bid->id }}">
+                    <img src="{{ URL::asset('img/subasta/'.$bid->imagen) }}" alt="" class="main_image"/>
+                  </a>
+                </li>
+                @endforeach
+              </ul>
+            </div>
+            <a class="buttons next" href="#">&#62;</a>
+          </div>
+        </div>
+      </div>
+
+
+
+      <div class="col-xs-3">
+
         <div class="col-xs-12" class="small_row"></div>
       </div>
       <div class="col-xs-8" >
         <h2 class="text-center">Noticias</h2>
+        <div class="col-xs-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt...
+        </div>
+        <div class="col-xs-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt...
+        </div>
         <div class="col-xs-4" id="slider5">
             <div class="viewport">
               <ul class="overview">
@@ -76,14 +87,6 @@
                 @endforeach
               </ul>
             </div>
-        </div>
-        <div class="col-xs-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt...
-        </div>
-        <div class="col-xs-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt...
         </div>
       </div>
       </div>
