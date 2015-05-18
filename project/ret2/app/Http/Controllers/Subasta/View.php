45aca98e285cce48dfcf5ebea5a8988e47c913d1
TroyerPro <frikiland2@gmail.com>
2015-05-18 17:07:38 +0200

    sdsdf
    dsfsdfsdf

--- project/ret2/app/Http/Controllers/Subasta/View.php
+++ project/ret2/app/Http/Controllers/Subasta/View.php
@@ -1,11 +1,13 @@
 <?php namespace App\Http\Controllers\Subasta;
 use Carbon\Carbon;
 use App\Subasta;
+use App\Http\Controllers\System\SystemController;
 use App\Puja;
 use App\User;
 use App\Http\Controllers\Controller;
 
 class View extends Controller {
+
 
 	public function __construct()
 	{
@@ -14,7 +16,9 @@ class View extends Controller {
 
 	public function show($id)
 	{
+		$system = new SystemController;
 		$subasta = Subasta::find($id);
+		$system->checkSubasta($subasta->id);
 		$pujas = Puja::select('users.name','pujas.cantidad','pujas.fecha')->where('pujas.id_subasta',$id)->join('users','pujas.id_usuario','=','users.id')->get();
 		$user = User::find($subasta->id_user_vendedor);
 		$fechaFinal = $subasta->fecha_final;
--- project/ret2/app/Http/Controllers/System/SystemController.php
+++ project/ret2/app/Http/Controllers/System/SystemController.php
@@ -1,14 +1,27 @@
 <?php namespace App\Http\Controllers\System;
 
 use App\Subasta;
+use Carbon\Carbon;
+use App\Http\Controllers\Controller;
 
 class SystemController extends Controller {
 
 
+  public function __construct()
+	{
+		$this->middleware('auth', [ 'except' => [ 'checkSubasta' ] ]);
+	}
 
 	public function checkSubasta($id)
 	{
     $subasta = Subasta::find($id);
+    $fechaFin = $subasta->fecha_final;
+    $fechaActual = Carbon::now();
+
+    if($fechaFin<$fechaActual) {
+      DD($fechaActual);
+      die();
+    }
 
 	}
 
