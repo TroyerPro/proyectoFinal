b5b684a8a9730046d2f560e2947c1ad313e44eb5
Sergio González Gómez <sergiogg1337@gmail.com>
2015-05-25 00:30:13 +0200

    asdasd
    sadasd

--- project/ret2/resources/views/pages/home.blade.php
+++ project/ret2/resources/views/pages/home.blade.php
@@ -71,7 +71,7 @@
   						<div class="col-lg-4 col-md-4 col-sm-4">
   							<a href="#" class="d_block animate_ftb h_md_auto m_xs_bottom_15 banner_type_2 r_corners red t_align_c tt_uppercase vc_child n_sm_vc_child">
   								<span class="d_inline_middle">
-  									<img class="d_inline_middle m_md_bottom_5" src="images/banner_img_3.png" alt="">
+  									<img class="d_inline_middle m_md_bottom_5" src="{{ URL::asset('img/banner_img_3.png') }}" alt="">
   									<span class="d_inline_middle m_left_10 t_align_l d_md_block t_md_align_c">
   										<b>Confía en nosotros</b><br><span class="color_dark">100% Garantía</span>
   									</span>
@@ -81,9 +81,9 @@
   						<div class="col-lg-4 col-md-4 col-sm-4">
   							<a href="#" class="d_block animate_ftb h_md_auto m_xs_bottom_15 banner_type_2 r_corners green t_align_c tt_uppercase vc_child n_sm_vc_child">
   								<span class="d_inline_middle">
-  									<img class="d_inline_middle m_md_bottom_5" src="images/banner_img_4.png" alt="">
+  									<img class="d_inline_middle m_md_bottom_5" src="{{ URL::asset('img/banner_img_4.png') }}" alt="">
   									<span class="d_inline_middle m_left_10 t_align_l d_md_block t_md_align_c">
-  										<b>Numero UNO</b><br><span class="color_dark">En casas de subastas</span>
+  										<b>Recibe la compra</b><br><span class="color_dark">¡Sin moverte!</span>
   									</span>
   								</span>
   							</a>
@@ -91,9 +91,9 @@
   						<div class="col-lg-4 col-md-4 col-sm-4">
   							<a href="#" class="d_block animate_ftb h_md_auto banner_type_2 r_corners orange t_align_c tt_uppercase vc_child n_sm_vc_child">
   								<span class="d_inline_middle">
-  									<img class="d_inline_middle m_md_bottom_5" src="images/banner_img_5.png" alt="">
+  									<img class="d_inline_middle m_md_bottom_5" src="{{ URL::asset('img/banner_img_5.png') }}" alt="">
   									<span class="d_inline_middle m_left_10 t_align_l d_md_block t_md_align_c">
-  										<b>¡Grandes ofertas!</b><br><span class="color_dark">¡Compra o vende!</span>
+  										<b>¡Grandes ofertas!</b><br><span class="color_dark">¡Compra un regalo!</span>
   									</span>
   								</span>
   							</a>
