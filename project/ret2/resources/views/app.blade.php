948b6d6596b430b9a5158e37e08c4f8f17d8f619
Sergio González Gómez <sergiogg1337@gmail.com>
2015-05-28 00:12:28 +0200

    limpiando mierrrrrrrda

--- project/ret2/resources/views/app.blade.php
+++ project/ret2/resources/views/app.blade.php
@@ -36,13 +36,6 @@
     DESCARGAR:
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'> -->
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
     <link rel="shortcut icon" href="{{{ asset('assets/site/ico/favicon.ico') }}}">
 </head>
 <body>
@@ -53,17 +46,12 @@
 @yield('content')
 </div>
 
-<!--
-Footer deshabilitado temporalmente
-@include('partials.footer')
--->
-<!-- Scripts -->
+
 <script src="{{ asset('/js/all.js') }}"></script>
 <script src="{{ asset('/js/jquery.autocomplete.js') }}"></script>
 <script src="{{ asset('/js/canvasjs.min.js') }}"></script>
 {{--<script src="{{ elixir('js/all.js') }}"></script>--}}
 
-{{-- TODO: Incorporate into elixir workflow. --}}
 <script src="{{asset('assets/site/js/jquery.justifiedGallery.min.js')}}"></script>
 <script src="{{asset('assets/site/js/lightbox.min.js')}}"></script>
 
