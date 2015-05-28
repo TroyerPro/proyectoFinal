e854788f2d97942e658e62575e559b4cb8feade5
Javier <javitobufo@gmail.com>
2015-05-28 13:36:18 +0200

    Corrección testing

--- project/ret2/resources/views/partials/nav.blade.php
+++ project/ret2/resources/views/partials/nav.blade.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
             </button>
             <!--    <a class="navbar-brand" href="{!! URL::to('/') !!}"> <img src="{{ asset('img/logomini.png') }}" /></a>-->
-            <a class="navbar-brand" href="{!! URL::to('/') !!}"><img class="logo" src="{{ URL::asset('img/logomini.png') }}"></a>
+            <a class="navbar-brand" href="{!! URL::to('/') !!}">RET</a>
         </div>
 
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
